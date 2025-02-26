a54449cd7a0c6980cda952dbbd1ef905aad2d633
Eric Savage <savage301@gmail.com>
2025-02-26 12:05:32 -0500

    Add ability to pull pit data too

--- Scouting Laptop Files/runThisToPullData.cmd
+++ Scouting Laptop Files/runThisToPullData.cmd
@@ -2,4 +2,5 @@
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
 For /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set mytime=%%a%%b)
 adb wait-for-device pull /sdcard/data.csv data@%mydate%_%mytime%.csv
+adb wait-for-device pull /sdcard/data_pit.csv data_pit@%mydate%_%mytime%.csv
 adb kill-server
